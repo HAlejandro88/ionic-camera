2301062612dbaa2fa88372b74677978b8176ecf8
Alejandro <alexsotog82@gmail.com>
2019-11-27 13:58:28 -0600

    camera-incompleta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Camera } from '@ionic-native/camera/ngx';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,6 +10,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  foto: any;
+
+  constructor(private camera: Camera) {}
+
+  hacerFoto() {
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL
+    }
+    this.camera.getPicture(options).then((ImageData) => {
+      this.foto = 'data:image/jpeg;base64' + ImageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  capturarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(imagenData => {
+      this.foto = 'data:image/jpeg;base64' + imagenData;
+    });
+  }
+
+  buscarFoto() {
+    this.camera.getPicture({
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(imagenData => this.foto = 'data:image/jpeg;base64' + imagenData );
+  }
 
 }
